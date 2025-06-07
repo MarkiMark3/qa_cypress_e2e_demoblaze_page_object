ddbc5efc391a513eff26ed20e090f3aebff0aae2
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2025-06-07 12:51:24 +0200

    tests

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -1,11 +1,41 @@
 /// <reference types='cypress' />
+import { faker } from '@faker-js/faker';
+import CheckoutFormPageObject from '../support/pages/checkoutForm.pageObject';
+import HomeAndCataloguePageObject
+  from '../support/pages/homeCatalogue.pageObject';
 
-describe('', () => {
+const homePage = new HomeAndCataloguePageObject();
+const checkout = new CheckoutFormPageObject();
+
+const testData = {
+  name: faker.person.firstName(),
+  country: faker.location.country(),
+  city: faker.location.city(),
+  card: faker.finance.creditCardNumber(),
+  month: new Date().toLocaleString('default', { month: 'long' }),
+  year: new Date().getFullYear()
+};
+describe('Checkout', () => {
   before(() => {
-
+    homePage.visit();
   });
 
-  it('', () => {
+  it('Should correctly checkout the product', () => {
+    homePage.clickOnCategory('Laptops');
+    homePage.clickOnProduct('Sony vaio i7');
+    cy.get('.btn-success').click();
+    homePage.clickOnLink('Cart');
+    cy.get('.btn-success').click();
 
+    checkout.typeName(testData.name);
+    checkout.typeCountry(testData.country);
+    checkout.typeCity(testData.city);
+    checkout.typeCredit(testData.card);
+    checkout.typeMonth(testData.month);
+    checkout.typeYear(testData.year);
+
+    checkout.clickOnPurchaseBtn();
+
+    cy.get('.confirm').click();
   });
 });
